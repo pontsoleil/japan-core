3130a2dfefe0d19ec906b6c9808e55834583554b
SAMBUICHI, Nobuyuki <nobuyuki@sambuichi.jp>
2023-01-25 09:24:03 +0900

    Update Schema2sequence.java

--- src/wuwei/japan_core/cius/Schema2sequence.java
+++ src/wuwei/japan_core/cius/Schema2sequence.java
@@ -1,28 +1,41 @@
 package wuwei.japan_core.cius;
 
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
 import org.w3c.dom.Node;
 
+import wuwei.japan_core.utils.CSV;
+
 /**
  * デジタルインボイスのXMLインスタンス文書を読み取り、Tidy dataを格納しているCSVファイルを出力する.
  */
 public class Schema2sequence {
-	static String XML_SCHEMA = "SMEinvoice/SME/schema/data/standard/ReusableAggregateBusinessInformationEntity_31p0.xsd";
-	static String ROOT_TYPE = "SMEinvoiceType";
-	static String OUT_CSV = "CIUS/data/csv/RAM.csv";
+	static String UBL_XML_SCHEMA = "UBL-2.1/xsd/maindoc/UBL-Invoice-2.1.xsd";
+	static String UBL_ROOT_TYPE = "InvoiceType";
+	static String UBL_OUT_CSV = "CIUS/data/csv/UBL-Invoice-2.1.csv";
+	
+	static String CII_XML_SCHEMA = "SMEinvoice/SME/schema/data/standard/SMEinvoice.xsd";
+	static String CII_ROOT_TYPE = "SMEinvoiceType";
+	static String CII_OUT_CSV = "CIUS/data/csv/SMEinvoice_ram.csv";
+	
+	static String JP_PINT_XPATH = "CIUS/data/base/JP_PINT_XPath.txt";
+	
+	static String PROCESSING;
+	
 	static String CHARSET = "UTF-8";
 
-	/**
-	 * XML SchemaのcomplexTyleのsequenceを指定する索引データ
-	 */
-    static TreeMap<String/* complex */,String/* element */> sequenceMap = new TreeMap<>();
+//	/**
+//	 * XML SchemaのcomplexTyleのsequenceを指定する索引データ
+//	 */
+//    static HashMap<String/* complex */,String/* element */> sequenceMap = new HashMap<>();
 
 	/**
  	 * mainでは変換処理を単体でテストする.
@@ -30,49 +43,67 @@ public class Schema2sequence {
 	 * @param args
 	 */
     public static void main(String[] args) {
-		 processSchema(XML_SCHEMA, ROOT_TYPE, OUT_CSV);
-		 System.out.println("** END Schema2sequence**");
+    	PROCESSING = "UBL";
+    	FileHandler.PROCESSING = PROCESSING;
+    	if ("UBL"==PROCESSING) {
+    		processSchema(UBL_XML_SCHEMA, UBL_ROOT_TYPE, UBL_OUT_CSV);
+    	} else if ("CII"==PROCESSING) {
+    		processSchema(CII_XML_SCHEMA, CII_ROOT_TYPE, CII_OUT_CSV);
+    	}
+		System.out.println("** END Schema2sequence**");
 	}
-	
+    
 	/**
-	 * XML Schemaを読み込んで sequenceMapに展開し、CSVファイルに出力する.
+	 * XML Schemaを読み込んで CSVファイルに出力する.
 	 * 
 	 * @param in_xml XMLスキーマ.
+	 * @param root_type スキーマのルート型.
 	 * @param out_csv complexTypeのXPathのsequenceのCSV(RFC4180形式)ファイル.
 	 */
 	private static void processSchema(String in_xml, String root_type, String out_csv) {
-		String root_xpath = "/xsd:schema/xsd:complexType[@name='"+root_type+"')]/xsd:sequence/xsd:element";
-	    sequenceMap = new TreeMap<String/* complex */,String/* element */>();
-
-		System.out.println("** processInvoice("+in_xml+"\n"+root_xpath+"\n"+out_csv+")");
+		String charset = "UTF-8";
+//	    List<String> xPathCounter = FileHandler.xPathCounter;
+		ArrayList<ArrayList<String>> data = new ArrayList<>();
+		
+		System.out.println("** processInvoice\n\t"+in_xml+"\n\t"+root_type+"\n\t"+out_csv);
 
 		try {
-			FileHandler.parseSchema(in_xml);
+			FileHandler.fillXPath(JP_PINT_XPATH); // FileHandler.jp_pint_XPath
+			
+			FileHandler.parseSchema(in_xml,root_type); // FileHandler.xPathCounter
+			
+			FileHandler.updateXPath();
+			
+			System.out.println("- FileHandler.csvFileWrite " + out_csv + " " + charset);
+			FileOutputStream fileOutputStream = new FileOutputStream(out_csv);
+			// header
+			ArrayList<String> header = null;
+			if ("CII"==PROCESSING) {
+				header = new ArrayList<>(Arrays.asList("seq", "UNID", "XPath"));
+			} else if ("UBL"==PROCESSING) {
+				header = new ArrayList<>(Arrays.asList("seq", "XPath"));
+			}
+			data.add(header);
+			// row
+			for (int i = 0; i < FileHandler.xPathCounter.size(); i++) {
+				String strI = ""+i;
+				String xPath = FileHandler.xPathCounter.get(i);
+				ArrayList<String> row = null;
+				if ("CII"==PROCESSING) {
+					String unid = xPath.substring(0,10);
+					xPath = xPath.substring(11);
+					row = new ArrayList<>(Arrays.asList(strI, unid, xPath));
+				} else if ("UBL"==PROCESSING) {
+					row = new ArrayList<>(Arrays.asList(strI, xPath));
+				}
+				data.add(row);
+			}
+			CSV.writeFile(fileOutputStream, data, charset);
+	        fileOutputStream.close();  
 		} catch (Exception e) {
 			e.printStackTrace();
 			return;
-		}
-		
-		List<Node> nodes =FileHandler.getXPathNodes(FileHandler.root, root_xpath);
-
-        Integer countNodes = nodes.size();             
-        if (countNodes > 0) {
-            for (int i = 0; i < countNodes; i++) {
-            	Node node = nodes.get(i);
-    			String value = node.getTextContent().trim();
-    			System.out.println(value);
-            }
-        }
-	    
-        try {
-			FileHandler.csvFileWrite(out_csv, CHARSET);
-        } catch (FileNotFoundException fnf) {
-        	System.out.println("* File Not Found Exception "+out_csv);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
+		}        
 		System.out.println("-- END -- "+in_xml);
 	}
-	
 }
